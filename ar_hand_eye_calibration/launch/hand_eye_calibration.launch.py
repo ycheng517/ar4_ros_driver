14f198e9a5ffb4ecc8dc7691efa4755eaf2f7d98
Yifei Cheng <ycheng517@gmail.com>
2024-04-16 08:46:37 -0400

    bug fixes

--- ar_hand_eye_calibration/launch/hand_eye_calibration.launch.py
+++ ar_hand_eye_calibration/launch/hand_eye_calibration.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument
 
@@ -19,11 +20,13 @@ def generate_launch_description():
                          "launch", "rs_launch.py")
         ]))
 
-    aruco_recognition_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory("ros2_aruco"), "launch",
-                         "aruco_recognition.launch.py")
-        ]))
+    aruco_params = os.path.join(
+        get_package_share_directory("ar_hand_eye_calibration"), "config",
+        "aruco_parameters.yaml")
+    aruco_recognition_node = Node(package='ros2_aruco',
+                                  executable='aruco_node',
+                                  parameters=[aruco_params])
+
     ar_hardware_interface_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory("ar_hardware_interface"),
@@ -34,7 +37,7 @@ def generate_launch_description():
             "calibrate": calibrate_arm,
         }.items())
     calibration_aruco_publisher = Node(
-        package="ar_hardware_interface",
+        package="ar_hand_eye_calibration",
         executable="calibration_aruco_publisher.py",
         name="calibration_aruco_publisher",
         output="screen",
@@ -55,6 +58,16 @@ def generate_launch_description():
                          "launch", "calibrate.launch.py")
         ]),
         launch_arguments=calibration_args.items())
+    easy_handeye2_delayed_launch = TimerAction(
+        period=5.0,  # Delay time in seconds
+        actions=[easy_handeye2_launch],
+        condition=UnlessCondition(calibrate_arm))
+    # If calibrating the arm, delay the launch of the handeye calibration
+    # until the arm is calibrated.
+    easy_handeye2_longer_delayed_launch = TimerAction(
+        period=45.0,  # Delay time in seconds
+        actions=[easy_handeye2_launch],
+        condition=IfCondition(calibrate_arm))
 
     ld = LaunchDescription()
     ld.add_action(
@@ -62,8 +75,9 @@ def generate_launch_description():
                               default_value="True",
                               description="Calibrate the arm"))
     ld.add_action(rs_launch)
-    ld.add_action(aruco_recognition_launch)
+    ld.add_action(aruco_recognition_node)
     ld.add_action(ar_hardware_interface_launch)
     ld.add_action(calibration_aruco_publisher)
-    ld.add_action(easy_handeye2_launch)
+    ld.add_action(easy_handeye2_delayed_launch)
+    ld.add_action(easy_handeye2_longer_delayed_launch)
     return ld
